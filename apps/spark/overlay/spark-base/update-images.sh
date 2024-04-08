76c0f2017b5f1f5e3e6d397bf197e5245772e2b7
Nataniel Almeida <natan.dias1@live.com>
2024-04-08 17:43:43 +0100

    Add new spark deployment and new scripts

--- apps/spark/overlay/spark-base/update-images.sh
+++ apps/spark/overlay/spark-base/update-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# USAGE: sh ./remove-images OLD_TAG NEW_TAG
+# USAGE: sh ./update-images.sh OLD_TAG NEW_TAG
 
 docker exec -it $(kind get clusters | head -1)-worker2 crictl rmi natandias1/spark-base:$1
 docker exec -it $(kind get clusters | head -1)-worker crictl rmi natandias1/spark-base:$1
