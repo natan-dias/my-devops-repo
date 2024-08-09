25727505750e44c4e1ab1d432dbf3100671bce85
Nataniel Almeida <natan.dias1@live.com>
2024-08-09 12:13:43 +0100

    update script and test 04

--- scripts/python_auto_pr_approve.py
+++ scripts/python_auto_pr_approve.py
@@ -12,7 +12,7 @@ gh = Github(github_token)
 
 # Get the repository and pull request from the github context
 repository = gh.get_repo(os.getenv('GITHUB_REPOSITORY'))
-pull_request_number = os.getenv('PULL_REQUEST_NUMBER')
+pull_request = os.getenv('PULL_REQUEST_NUMBER')
 #os.getenv('GITHUB_REF').split('/')[-1]
 #if pull_request_number != 'merge':
 #    pull_request = repository.get_pull(int(pull_request_number))
