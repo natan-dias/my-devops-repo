701685a89ade8af15f1127fa16dc003130382cd8
Natan Dias <30359003+natan-dias@users.noreply.github.com>
2024-12-05 11:48:14 +0000

    add basic video store api (#34)
    * add basic video store api
    
    * fix debug mode

---
+++ apps/basic-video-store-api-with-db/main.py
@@ -0,0 +1,101 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+#with app.app_context():
+#    db.create_all()
+
+#small healthcheck test when call /healthcheck
+class healthcheck(Resource):
+    def get(self):
+        return {'hello': 'healthy'}
+
+api.add_resource(healthcheck, '/healthcheck')
+#--------------------------------------------#
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video", required=False)
+video_update_args.add_argument("views", type=str, help="Views of the video", required=False)
+video_update_args.add_argument("likes", type=str, help="Likes on the video", required=False)
+
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
+class Video(Resource):
+    @marshal_with(resource_fields)
+    def get(self, video_id):
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
+        return result
+    
+    @marshal_with(resource_fields)
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        id_check = VideoModel.query.filter_by(id=video_id).first()
+        if id_check:
+            abort(409, message="Video ID already exists")
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+        
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
+        
+        if args["name"]:
+            result.name = args["name"]
+        if args["views"]:
+            result.views = args["views"]
+        if args["likes"]:
+            result.likes = args["likes"]
+   
+        db.session.commit()
+
+        return result
+    
+
+    def delete(self, video_id):
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id")
+        db.session.delete(result)
+        db.session.commit()
+        log = {"message": "Deleted video with id {}".format(video_id)}
+        return log, 200
+    
+api.add_resource(Video, "/video/<int:video_id>")
+
+#--------------------------------------------#
+
+if __name__ == '__main__':
+    debug_mode = os.environ.get("DEBUG_MODE", "false").lower() == "true"
+    app.run(debug=debug_mode)
