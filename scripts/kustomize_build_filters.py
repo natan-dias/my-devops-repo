b29b7c9e2e5e5eeb57ceeb6dd631e49bc4ba7683
Natan Dias <natan.dias1@live.com>
2025-02-20 17:08:41 +0000

    Update Kustomize Build script
    This version of the script now can interact with a list "DEFAULT_KINDS" and search for everything related to a given
    metadata.name you provide using "-n" option.
    
    You can also select more than one kind to search in your kustomize build command.

--- scripts/kustomize_build_filters.py
+++ scripts/kustomize_build_filters.py
@@ -1,17 +1,39 @@
 import yaml
 import subprocess
+import argparse
 
-# Define the path to the kustomization file
-kustomization_path = "PATH"
+DEFAULT_KINDS = ["Deployment", "Service", "Ingress", "Job", "CronJob", "StatefulSet", "DaemonSet", "Pod", "ConfigMap", "Secret", "ServiceAccount", "Role", "RoleBinding", "ClusterRole"]
 
-# Run kustomize build with the specified path
-output = subprocess.check_output(["kustomize", "build", kustomization_path])
+def kustomize_build_filter(path, name, kinds=None):
 
-# Parse the YAML output
-resources = yaml.safe_load_all(output)
+    try:
+        output = subprocess.check_output(["kustomize", "build", path])
+    except subprocess.CalledProcessError as e:
+        print(f"Error running kustomize build: {e}")
+        return
 
-# Iterate over the resources and find the desired one
-for resource in resources:
-    if resource["kind"] == "Deployment" and resource["metadata"]["name"] == "DEPLOY":
-        print(yaml.dump(resource))
-        break
+    resources = yaml.safe_load_all(output)
+
+    if kinds is None or kinds == []:
+        kinds = DEFAULT_KINDS
+
+    found_kinds = set()
+    for resource in resources:
+        if resource["metadata"]["name"] == name and resource["kind"] in kinds:
+            print(yaml.dump(resource))
+            found_kinds.add(resource["kind"])
+
+    for kind in kinds:
+        if kind not in found_kinds:
+            print(f"Error: Resource kind '{kind}' not found for name '{name}'")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", "-p", type=str, required=True, help="Path to the kustomization file")
+    parser.add_argument("--name", "-n", type=str, required=True, help="Name of the resource")
+    parser.add_argument("--kind", "-k", type=str, action="append", help="Kind of the resource")
+
+    args = parser.parse_args()
+
+    kustomize_build_filter(args.path, args.name, args.kind)
